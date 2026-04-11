8f6829444fcfeaa13596d3a8437b7a952d4f5283
Joshua Groen <josh.groen@gmail.com>
2026-04-11 14:55:23 -0400

    Refactor DOA estimation and signal processing

--- Lesson32/group3.py
+++ Lesson32/group3.py
@@ -1,93 +1,75 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-# ================================
-# PARAMETERS (students will edit one line)
-# ================================
-sample_rate = 1e6
-N = 10000
-d = 0.5
-Nr = 8
+# Array params
+center_freq = 3.3e9
+sample_rate = 30e6
+d = 0.045 * center_freq / 3e8
+print("d:", d)
 
-theta1 = 20 / 180 * np.pi   # interferer
-theta2 = 25 / 180 * np.pi   # interferer
-theta3 = -40 / 180 * np.pi  # signal of interest (SOI)
+# Includes all three signals, we'll call C our SOI
+filename = '3p3G_A_B_C.npy'
+X = np.load(filename)
+Nr = X.shape[0]
 
-mask_fraction = 0.4   # <-- STUDENTS CHANGE THIS (try 0.2, 0.6, etc.)
-
-# ================================
-# SIGNAL GENERATION
-# ================================
-t = np.arange(N)/sample_rate
-
-s1 = np.exp(2j * np.pi * d * np.arange(Nr) * np.sin(theta1)).reshape(-1,1)
-s2 = np.exp(2j * np.pi * d * np.arange(Nr) * np.sin(theta2)).reshape(-1,1)
-s3 = np.exp(2j * np.pi * d * np.arange(Nr) * np.sin(theta3)).reshape(-1,1)
-
-tone1 = np.exp(2j*np.pi*0.01e6*t).reshape(1,-1)
-tone2 = np.exp(2j*np.pi*0.02e6*t).reshape(1,-1)
-tone3 = np.exp(2j*np.pi*0.03e6*t).reshape(1,-1)
-
-# Interferers are stronger than SOI
-X = 2*s1 @ tone1 + 2*s2 @ tone2 + 0.5*s3 @ tone3
-
-n = np.random.randn(Nr, N) + 1j*np.random.randn(Nr, N)
-X = X + 0.05*n
-
-# ================================
-# MVDR FUNCTION
-# ================================
-def power_mvdr(theta, X):
-    s = np.exp(2j * np.pi * d * np.arange(X.shape[0]) * np.sin(theta)).reshape(-1,1)
-    R = (X @ X.conj().T)/X.shape[1]
-    Rinv = np.linalg.pinv(R)
-    return 1/(s.conj().T @ Rinv @ s).squeeze()
-
-# ================================
-# SCAN (DOA ESTIMATION)
-# ================================
-theta_scan = np.linspace(-np.pi/2, np.pi/2, 1000)
-
+# Perform DOA to find angle of arrival of C
+theta_scan = np.linspace(-1*np.pi/2, np.pi/2, 10000)
 results = []
-for theta in theta_scan:
-    p = power_mvdr(theta, X)
-    results.append(10*np.log10(np.abs(p)))
+
+R = X @ X.conj().T
+Rinv = np.linalg.pinv(R)
+
+for theta_i in theta_scan:
+    a = np.exp(2j * np.pi * d * np.arange(Nr) * np.sin(theta_i))
+    a = a.reshape(-1,1)
+    power = 1/(a.conj().T @ Rinv @ a).squeeze()
+    power_dB = 10*np.log10(np.abs(power))
+    results.append(power_dB)
 
 results = np.array(results)
 results -= np.max(results)
 
-# ================================
-# TRAINING DATA SELECTION (MASKING)
-# ================================
+# Pull out angle of C (THIS IS THE STUDENT MODIFICATION POINT)
 results_temp = np.array(results)
+results_temp[int(len(results)*0.4):] = -9999*np.ones(int(len(results)*0.6))
+max_angle = theta_scan[np.argmax(results_temp)]
+print("max_angle:", max_angle)
 
-cutoff = int(len(results) * mask_fraction)
-results_temp[cutoff:] = -9999  # mask out part of angle space
+# Calc MVDR weights (NO TRAINING DATA)
+s = np.exp(2j * np.pi * d * np.arange(Nr) * np.sin(max_angle))
+s = s.reshape(-1,1)
+w = (Rinv @ s)/(s.conj().T @ Rinv @ s)
 
-# Estimate SOI angle
-theta_est = theta_scan[np.argmax(results_temp)]
+# Calc beam pattern
+w = w.squeeze()
+N_fft = 2048
+w_padded = np.concatenate((w, np.zeros(N_fft - Nr)))
+w_fft_dB = 10*np.log10(np.abs(np.fft.fftshift(np.fft.fft(w_padded)))**2)
+w_fft_dB -= np.max(w_fft_dB)
+theta_bins = np.arcsin(np.linspace(-1, 1, N_fft))
 
-print("Estimated SOI angle (deg):", theta_est * 180 / np.pi)
-
-# ================================
-# PLOTS
-# ================================
-plt.figure(figsize=(10,4))
-
-plt.subplot(1,2,1)
-plt.plot(theta_scan*180/np.pi, results)
-plt.axvline(theta_est*180/np.pi, color='r', linestyle='--', label="Estimated SOI")
-plt.title("DOA Spectrum")
-plt.xlabel("Theta [Degrees]")
-plt.ylabel("Power (dB)")
-plt.legend()
+plt.plot(theta_bins * 180 / np.pi, w_fft_dB)
+plt.plot(theta_scan * 180 / np.pi, results, 'r')
+plt.vlines(ymax=np.max(results),
+           ymin=np.min(results),
+           x=max_angle*180/np.pi,
+           color='g', linestyle='--')
+plt.xlabel("Angle [deg]")
+plt.ylabel("Magnitude [dB]")
+plt.title("Beam Pattern and DOA Results, Without Training")
 plt.grid()
-
-plt.subplot(1,2,2)
-plt.plot(theta_scan*180/np.pi, results_temp)
-plt.title("Masked Spectrum (Training Data Selection)")
-plt.xlabel("Theta [Degrees]")
-plt.grid()
-
-plt.tight_layout()
 plt.show()
+
+# ================================
+# TRAINING DATA (A + B ONLY)
+# ================================
+filename = '3p3G_A_B.npy'
+X_A_B = np.load(filename)
+
+R_training = X_A_B @ X_A_B.conj().T
+Rinv_training = np.linalg.pinv(R_training)
+
+# Calc MVDR weights using training covariance
+s = np.exp(2j * np.pi * d * np.arange(Nr) * np.sin(max_angle))
+s = s.reshape(-1,1)
+w_training = (Rinv_training @ s)/(s.conj().T @ Rinv_training @ s)
